c0be6582ecd2e2f756c126c8e74aacc7c62ad187
hszabina <herbely.szabina@gmail.com>
2017-04-11 14:35:12 +0200

    Revert "fecccs"
    This reverts commit dc21f4ae80a879fc907e26c72128fe94054a2004.

--- app/pokemon.js
+++ app/pokemon.js
@@ -1,9 +1,9 @@
 
-/*function reqListener () {
+function reqListener () {
     var container = $(".pocemon-container");
     container.empty();
 
-    var response = JSON.parse(this.response);
+    var response = JSON.parse(this.response)
 
     $.each(response.results, function(index, value){
         var item = document.createElement("div");
@@ -22,25 +22,4 @@
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
 oReq.open("GET", "http://pokeapi.co/api/v2/pokemon/?limit=200");
-oReq.send();*/
-
-fetch('http://pokeapi.co/api/v2/pokemon/?limit=200')
-.then(function(response) {
-  var container = $(".pocemon-container");
-  container.empty();
-  return response.json()
-.then(function(list) {
-   $.each(list.results, function(index, value){
-        var item = document.createElement("div");
-        $(item).addClass("pocemon-item");
-        var img = document.createElement("img");
-        var id = value.url.replace("http://pokeapi.co/api/v2/pokemon/", "").replace("/", "");
-        $(img).attr("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png");
-        $(img).addClass("kisdogok");
-        var title = document.createElement("span");
-        title.textContent = value.name;
-        item.append(img, title);
-        container.append(item);
-    });
-})
-});
+oReq.send();
