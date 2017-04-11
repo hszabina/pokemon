c305e2f174930f2fd63d75647ac9886e6da48c53
hszabina <herbely.szabina@gmail.com>
2017-04-11 13:04:53 +0200

    csak

--- app/pokemon.js
+++ app/pokemon.js
@@ -1,48 +1,25 @@
-var list = [
-{
-  "id": 1,
-  "name": "Bulbasaur",
-  "type": [ "grass", "posion" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-},
-{
-  "id": 5,
-  "name": "Charmeleon",
-  "type": [ "fire" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-},
-{
-  "id": 15,
-  "name": "Beedrill",
-  "type": [ "bug", "posion" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
-},
-{
-  "id": 193,
-  "name": "Yanma",
-  "type": [ "bug", "flying" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
-},
-{
-  "id": 152,
-  "name": "Chikorita",
-  "type": [ "grass" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
-},
-];
 
-$( document ).ready(function() {
+function reqListener () {
     var container = $(".pocemon-container");
-    
-    $.each(list, function(index, value){
+    container.empty();
+
+    var response = JSON.parse(this.response)
+
+    $.each(response.results, function(index, value){
         var item = document.createElement("div");
         $(item).addClass("pocemon-item");
         var img = document.createElement("img");
-        $(img).attr("src", value.sprite);
+        var id = value.url.replace("http://pokeapi.co/api/v2/pokemon/", "").replace("/", "");
+        $(img).attr("src", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png");
         $(img).addClass("kisdogok");
         var title = document.createElement("span");
         title.textContent = value.name;
         item.append(img, title);
         container.append(item);
     });
-});
+};
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "http://pokeapi.co/api/v2/pokemon/?limit=200");
+oReq.send();
